a1f2e2776c8229b400b388546081f2a62d3b8998
Akshay Rajput <137922595+akshayrajputttt@users.noreply.github.com>
2024-05-29 10:24:53 +0530

    Add files via upload

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a>Awwwards</a>
+        <a>Instagram</a>
+        <a href="#">Github</a>
+        <a href="https://www.linkedin.com/in/akshaypundirr/">LinkedIn</a>
+    </div>
+  )
+}
